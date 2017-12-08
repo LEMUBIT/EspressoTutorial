4d9ca637a5078301769c7d1b04d45d83954d89cd
lemuelc7 <lemuelcco@gmail.com>
2017-12-08 13:07:28 +0100

    Use Lambda expression

--- app/src/main/java/com/lemuel/lemubit/espressotutorial/MainActivity.java
+++ app/src/main/java/com/lemuel/lemubit/espressotutorial/MainActivity.java
@@ -1,32 +1,33 @@
-package com.lemuel.lemubit.espressotutorial;
+    package com.lemuel.lemubit.espressotutorial;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+    import android.support.v7.app.AppCompatActivity;
+    import android.os.Bundle;
+    import android.text.TextUtils;
+    import android.widget.Button;
+    import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-    EditText editTextA;
-    EditText editTextB;
-    Button copyBtn;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        editTextA = findViewById(R.id.textA);
-        editTextB = findViewById(R.id.textB);
-        copyBtn = findViewById(R.id.copyBtn);
+    public class MainActivity extends AppCompatActivity {
+        EditText editTextA;
+        EditText editTextB;
+        Button copyBtn;
 
-        copyBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(!TextUtils.isEmpty(editTextA.getText().toString())) {
-                    editTextB.setText(editTextA.getText().toString());
-                }
-            }
-        });
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            editTextA = findViewById(R.id.textA);
+            editTextB = findViewById(R.id.textB);
+            copyBtn = findViewById(R.id.copyBtn);
+
+            copyBtn.setOnClickListener(
+                    (view -> {
+                        if (!TextUtils.isEmpty(editTextA.getText().toString())) {
+                            editTextB.setText(editTextA.getText().toString());
+                        }
+                    })
+            );
+
+        }
+
     }
-}
